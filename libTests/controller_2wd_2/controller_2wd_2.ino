f3125e194ef84eb5b18a81605f54b70a97f33964
D Zhelyazkov LT <d.zhelyazkov.7331@gmail.com>
2017-01-22 17:38:22 +0200

    Git ignore update + removing gitignore files. Fixed Controller2wd2 project.

--- libTests/controller_2wd_2/controller_2wd_2.ino
+++ libTests/controller_2wd_2/controller_2wd_2.ino
@@ -3,6 +3,7 @@
 
 #include "Controller2WDv2.h"
 
+
 //arduino motor shield pins
 #define A_DIR 12
 #define A_PWM 3
@@ -17,55 +18,62 @@
 #define MAX_POWER 255
 #define MIN_POWER 100
 
+#define MPU_INT_PIN 2
+
+
 MotorWithEncoder motorA(A_DIR, A_PWM, A_BRK, A_ENC);
 MotorWithEncoder motorB(B_DIR, B_PWM, B_BRK, B_ENC);
 
 MPUCompass* compass;
 Controller2WDv2* controller;
+
+
 void setup()
 {
-	Serial.begin(9600);
-	Serial.println("------------------PROGRAM START-------------");
+    Serial.begin(9600);
+    Serial.println("------------------PROGRAM START-------------");
 
-	motorA.setPower(MIN_POWER);
-	motorB.setPower(MIN_POWER);
+    motorA.setPower(MIN_POWER);
+    motorB.setPower(MIN_POWER);
 
-	compass = new MPUCompass();;
-	controller = new Controller2WDv2(&motorA, &motorB, compass);
-	//controller->learn();
+    compass = new MPUCompass(MPU_INT_PIN);
+    controller = new Controller2WDv2(&motorA, &motorB, compass);
+    //controller->learn();
 
-	Serial.println("Awaiting command");
+    Serial.println("Awaiting command");
 }
 
 void loop()
 {
-	delay(1000);
-	if (!Serial.available())
-		return;
+    delay(1000);
+    if (!Serial.available())
+        return;
 
-	String command = Serial.readString();
-	if (command.equalsIgnoreCase("learn")) {
-		controller->learn();
-	}
-	else if (command.equalsIgnoreCase("turn")) {
-		Serial.println("Give a degree.");
-		while (!Serial.available())
-			delay(1000);
+    String command = Serial.readString();
+    if (command.equalsIgnoreCase("learn")) {
+        compass->startListening();
+        controller->learn();
+        compass->stopListening();
+    }
+    else if (command.equalsIgnoreCase("turn")) {
+        Serial.println("Give a degree.");
+        while (!Serial.available())
+            delay(1000);
 
-		short degree = Serial.parseInt();
-		controller->turn(degree);
-	}
-	else if (command.equalsIgnoreCase("move")) {
-		Serial.println("How many ticks?");
-		while (!Serial.available())
-			delay(1000);
+        short degree = Serial.parseInt();
+        controller->turn(degree);
+    }
+    else if (command.equalsIgnoreCase("move")) {
+        Serial.println("How many ticks?");
+        while (!Serial.available())
+            delay(1000);
 
-		short ticks = Serial.parseInt();
-		controller->move(ticks);
-	}
-	else {
-		Serial.println("Command not matched correctly! Commands: [Learn,Turn, Move]\n");
-	}
+        short ticks = Serial.parseInt();
+        controller->move(ticks);
+    }
+    else {
+        Serial.println("Command not matched correctly! Commands: [Learn,Turn, Move]\n");
+    }
 
-	Serial.println("Awaiting command");
+    Serial.println("Awaiting command");
 }
